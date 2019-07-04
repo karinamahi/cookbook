205edaeb3c857972c160e2bf04c015eab8371100
karinamahi <karinahiratabueno@gmail.com>
2019-07-04 20:46:30 -0300

    :hammer:changing structure

--- frontend/src/main/components/RecipeDetails.vue
+++ frontend/src/main/components/RecipeDetails.vue
@@ -1,65 +1,31 @@
 <template>
-    <div>
-        <section>
-          <v-layout>
-            <v-flex xs12 sm12 md12 lg12>
-              <v-card ref="form">
-                <v-card-text>
-                  <v-text-field
-                    ref="name"
-                    v-model="recipe.name"
-                    :rules="[() => !!recipe.name || 'This field is required']"
-                    :error-messages="errorMessages"
-                    label="Name"
-                    required
-                  ></v-text-field>
+  <div>
+    <v-card-text>
+      <v-text-field
+        ref="name"
+        v-model="recipe.name"
+        :rules="[() => !!recipe.name || 'This field is required']"
+        :error-messages="errorMessages"
+        label="Name"
+        required
+      ></v-text-field>
 
-                  <v-text-field
-                    ref="description"
-                    v-model="recipe.description"
-                    :error-messages="errorMessages"
-                    label="Description"
-                    required
-                  ></v-text-field>
-                  
-                </v-card-text>
-                <v-divider class="mt-5"></v-divider>
-                <v-card-actions>
-                  <v-btn flat>Cancel</v-btn>
-                  <v-spacer></v-spacer>
-                  <v-slide-x-reverse-transition>
-                    <v-tooltip
-                      v-if="formHasErrors"
-                      left
-                    >
-                      <template v-slot:activator="{ on }">
-                        <v-btn
-                          icon
-                          class="my-0"
-                          @click="resetForm"
-                          v-on="on"
-                        >
-                          <v-icon>refresh</v-icon>
-                        </v-btn>
-                      </template>
-                      <span>Refresh form</span>
-                    </v-tooltip>
-                  </v-slide-x-reverse-transition>
-                  <v-btn color="primary" flat @click="submit">Submit</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-
-        </section>
-    </div>
+      <v-text-field
+        ref="description"
+        v-model="recipe.description"
+        :error-messages="errorMessages"
+        label="Description"
+        required
+      ></v-text-field>
+    </v-card-text>
+  </div>
 </template>
 
 
 <script>
 import Vue from "vue";
 import Vuetify from "vuetify";
- 
+
 Vue.use(Vuetify);
 export default Vue.extend({
   data: function() {
@@ -68,47 +34,44 @@ export default Vue.extend({
         name: "",
         description: ""
       },
-       errorMessages: '',
-       formHasErrors: false
+      errorMessages: "",
+      formHasErrors: false
     };
   },
 
   computed: {
-      form () {
-        return {
-          recipe: {}
-        }
-      }
-    },
-    watch: {
-      name () {
-        this.errorMessages = ''
-      }
-    },
-
-
-    methods: {
-     resetForm(){
-       console.log('Reseting...');
-        this.errorMessages = []
-        this.formHasErrors = false
-
-        Object.keys(this.form).forEach(f => {
-          this.$refs[f].reset()
-        })
-     },
-      submit(){
-        console.log('Submiting: ', this.recipe, this.errorMessages);
-        this.formHasErrors = false
-        
-
-        // Object.keys(this.form).forEach(f => {
-        //   if (!this.form[f]) this.formHasErrors = true
-
-        //   this.$refs[f].validate(true)
-        // })
-      }
+    form() {
+      return {
+        recipe: {}
+      };
     }
-  
+  },
+  watch: {
+    name() {
+      this.errorMessages = "";
+    }
+  },
+
+  methods: {
+    resetForm() {
+      console.log("Reseting...");
+      this.errorMessages = [];
+      this.formHasErrors = false;
+
+      Object.keys(this.form).forEach(f => {
+        this.$refs[f].reset();
+      });
+    },
+    submit() {
+      console.log("Submiting: ", this.recipe, this.errorMessages);
+      this.formHasErrors = false;
+
+      // Object.keys(this.form).forEach(f => {
+      //   if (!this.form[f]) this.formHasErrors = true
+
+      //   this.$refs[f].validate(true)
+      // })
+    }
+  }
 });
 </script>
--- frontend/src/main/pages/RecipeDetailsPage.vue
+++ frontend/src/main/pages/RecipeDetailsPage.vue
@@ -1,43 +1,69 @@
 <template>
-    <div>
-        <h1>Oi Page</h1>
-        <recipe-details></recipe-details>
+  <div>
+    <section>
+      <v-layout>
+        <v-flex xs12 sm12 md12 lg12>
+          <v-card ref="form">
+            <recipe-details></recipe-details>
 
-        
-        <v-container>
-          <h2>Aqui fica a lista de coisas</h2>
-          <stuff-card></stuff-card>
-        </v-container>
+            <v-divider class="mt-5"></v-divider>
 
-        <v-container>
-          <h2>Aqui ficam as etapas</h2>
-          <step-card></step-card>
-        </v-container>
-    </div>
+            <v-container>
+              <h2>Aqui fica a lista de coisas</h2>
+              <stuff-card></stuff-card>
+            </v-container>
+
+            <v-divider class="mt-5"></v-divider>
+            <v-container>
+              <h2>Aqui ficam as etapas</h2>
+              <step-card></step-card>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </section>
+
+    <v-card-actions>
+      <v-btn flat>Cancel</v-btn>
+      <v-spacer></v-spacer>
+      <v-slide-x-reverse-transition>
+        <v-tooltip v-if="formHasErrors" left>
+          <template v-slot:activator="{ on }">
+            <v-btn icon class="my-0" @click="resetForm" v-on="on">
+              <v-icon>refresh</v-icon>
+            </v-btn>
+          </template>
+          <span>Refresh form</span>
+        </v-tooltip>
+      </v-slide-x-reverse-transition>
+      <v-btn color="primary" flat @click="submit">Submit</v-btn>
+    </v-card-actions>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import RecipeDetails from "./../components/RecipeDetails.vue"
-import StepCard from "./../components/StepCard.vue"
-import StuffCard from "./../components/StuffCard.vue"
+import Vue from "vue";
+import RecipeDetails from "./../components/RecipeDetails.vue";
+import StepCard from "./../components/StepCard.vue";
+import StuffCard from "./../components/StuffCard.vue";
 
 export default Vue.extend({
-    components: {
-        RecipeDetails, StepCard, StuffCard
-    },
-    data: function(){
-      return {
-        tabs: null,
-        labels: ["RecipeDetails", "Settings", "About"],
-        current: "RecipeDetails",
-          
-      };
+  components: {
+    RecipeDetails,
+    StepCard,
+    StuffCard
+  },
+  data: function() {
+    return {
+      tabs: null,
+      labels: ["RecipeDetails", "Settings", "About"],
+      current: "RecipeDetails"
+    };
   },
   methods: {
-      setCurrent(label) {
-        this.current = label;
-      }
+    setCurrent(label) {
+      this.current = label;
+    }
   }
 });
 </script>
